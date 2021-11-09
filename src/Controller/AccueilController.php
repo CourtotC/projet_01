5451d97bf83bb5ccbc482394aed7b1196c921692
CourtotC <cedric.courtot@yahoo.fr>
2021-11-09 10:18:18 +0100

    html.twig 1ier exercice

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -2,12 +2,16 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 
-class AccueilController
+class AccueilController extends AbstractController
 {
+
+
+
 
  //#[Route("/", methods:"GET", name:"racine")]
 
@@ -17,25 +21,41 @@ class AccueilController
  * @Route("/", name="racine", methods={"GET"})
  *
  */
-    public function bonjour()
-    {
-       return new Response('Voici la page principale de COURTOT Cédric');
-    }
+   //  public function bonjour()
+   //  {
+   //     return new Response('Voici la page principale de COURTOT Cédric');
+   //  }
+
+
+
+
+
 
 /**
  * @Route("/produits/affiche-creneau-journee")
  *
  */
-    public function afficheCreneau()
-    {
-       return new Response("Future page d'affichage de créneaux");
-    }
+   //  public function afficheCreneau()
+   //  {
+   //     return new Response("Future page d'affichage de créneaux");
+   //  }
 
 
- #[Route("/produits/{joker}")]
-    public function afficheCreneautest($joker)
-    {
-       return new Response("Future page d'affichage de créneaux: {$joker}");
+
+//  #[Route("/produits/{joker}")]
+//     public function afficheCreneautest($joker)
+//     {
+//        return new Response("Future page d'affichage de créneaux: {$joker}");
+//     }
+
+
+
+
+
+
+    #[Route("/produits/{var}")]
+    function afficheCreneau($var){
+return $this->render('creneau/affiche.html.twig',['title'=>ucwords(str_replace('-',' ',$var))]);
     }
 
 
