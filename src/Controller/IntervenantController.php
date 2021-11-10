789d192adf15cfb46f924916d7a88093fa3c10de
CourtotC <cedric.courtot@yahoo.fr>
2021-11-10 11:20:44 +0100

    ajout d'intervenantController et intervenantformtype, creation newhtml.twig

--- src/Controller/IntervenantController.php
+++ src/Controller/IntervenantController.php
@@ -3,34 +3,42 @@
 namespace App\Controller;
 
 use App\Entity\Intervenant;
+use App\Form\IntervenantFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class IntervenantController extends AbstractController
 {
-  
-    #[Route("/user", methods:"GET")]
-    public function creationIntervenant()
-    {
-        $entityManager = $this ->getDoctrine()->getManager();
-        // Création d’une instance
-        // $inter = new Intervenant();
-        // $inter->setNom("Leclerc");
-        // $inter->setPrenom("Gautier");
-        // $inter->setSpecialiteprofessionnelle("Docteur en informatique");
-        $inter = new Intervenant();
-        $inter->setNom("Dubois");
-        $inter->setPrenom("Suzanne");
-        $inter->setSpecialiteprofessionnelle("Ingénieur systèmes");
+    // #[Route("/user", methods: "GET")]
+    // public function creationIntervenant()
+    // {
+    //     $entityManager = $this->getDoctrine()->getManager();
+    //     // Création d’une instance
+    //     // $inter = new Intervenant();
+    //     // $inter->setNom("Leclerc");
+    //     // $inter->setPrenom("Gautier");
+    //     // $inter->setSpecialiteprofessionnelle("Docteur en informatique");
+    //     $inter = new Intervenant();
+    //     $inter->setNom("Dubois");
+    //     $inter->setPrenom("Suzanne");
+    //     $inter->setSpecialiteprofessionnelle("Ingénieur systèmes");
 
-        // informer Doctrine que l'on veut persister ces données (ne provoque pas de Query)
-        $entityManager->persist($inter);
-        //  exécution de la requête (i.e. the INSERT query). Quand cette méthode est appelée
-        // Doctrine vérifie parmi les objets qu’elle gère s’ils doivent être persistés dans la
-        // bdd. En l’occurrence, un nouvel enregistrement est créé (Doctrine est capable de
-        // déterminer si elle doit INSERT ou UPDATE votre entité
-        $entityManager->flush();
-        return new Response('mise a jour ok');
+    //     // informer Doctrine que l'on veut persister ces données (ne provoque pas de Query)
+    //     $entityManager->persist($inter);
+    //     //  exécution de la requête (i.e. the INSERT query). Quand cette méthode est appelée
+    //     // Doctrine vérifie parmi les objets qu’elle gère s’ils doivent être persistés dans la
+    //     // bdd. En l’occurrence, un nouvel enregistrement est créé (Doctrine est capable de
+    //     // déterminer si elle doit INSERT ou UPDATE votre entité
+    //     $entityManager->flush();
+    //     return new Response('mise a jour ok');
+    // }
+
+    #[Route("/intervenant/create", name: "intervenant_create")]
+    public function new()
+    {
+        $task = new Intervenant();
+        $form = $this->createForm(IntervenantFormType::class, $task);
+        return $this->render('intervenant/new.html.twig', ['intervenantForm' => $form->createView(),]);
     }
 }
