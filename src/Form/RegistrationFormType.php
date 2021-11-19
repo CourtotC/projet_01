e69dd88948ecda993d89ac89004cb355d941d082
CourtotC <cedric.courtot@yahoo.fr>
2021-11-19 14:36:33 +0100

    mise a jour formulaire inscription avec email

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -21,6 +21,7 @@ class RegistrationFormType extends AbstractType
             ->add('username')
             ->add('nom')
             ->add('prenom')
+            ->add('email')
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
